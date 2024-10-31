3024e57c6033a96e749ddf96bb01db6791d74b1f
Arkaji <leonardo.jrm@gmail.com>
2024-10-31 14:27:38 -0500

    feat(Style): Configured part of the site's style and the navbar.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using BaseWebApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -12,7 +13,7 @@ namespace BaseWebApplication.Controllers
         {
             _logger = logger;
         }
-
+        [Authorize]
         public IActionResult Index()
         {
             return View();
