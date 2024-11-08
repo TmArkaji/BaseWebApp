fc0247709d855faef94fd2d990eaa7a016dd1db9
Arkaji <leonardo.jrm@gmail.com>
2024-11-08 11:32:32 -0500

    feat(site.js): Select2 for al the select input and replace dot with comma function (disable at the moment)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('select').select2({
+        width: 'resolve'
+    });
+
+    //$(".numeric-field").on('input', function (event) {
+    //    // existe en site.js
+    //    replaceDotWithComma(event);
+    //});
+});
+
+
+function replaceDotWithComma(event) {
+    // console.log("event.target.value: ", event.target.value);
+    event.target.value = event.target.value.replace('.', ',');
+}
